ab676033367695ad3326ca98723ee4a50f0f47d7
Rafael Antonio Lucio <rafaelantoniolucio@gmail.com>
2019-10-15 18:54:38 -0300

    configs(configs): add lerna orchestration monorepo

---
+++ packages/friday/__tests__/friday.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const friday = require('..');
+
+describe('friday', () => {
+    it('needs tests');
+});
---
+++ packages/friday/lib/friday.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = friday;
+
+function friday() {
+    // TODO
+}
